25b6cb1cbe9d620bc07651dc5f2a326cb47790cf
iamNIKHIL-dotcom <ummidinikhil@gmail.com>
2025-08-28 13:45:22 +0530

    mintTo done

--- 5.lst/src/index.ts
+++ 5.lst/src/index.ts
@@ -7,22 +7,25 @@ app.use(express.json());
 
 
 app.post("/helius",async (req,res)=>{
+    const {  toAddress, amount,type } = req.body;
 
-    const fromAddress = req.body.fromAddress;
-    const toAddress = req.body.toAddress;
-    const amount = req.body.amount;
-    const type = "received_native_sol";
+    // const type = "received_native_sol";
+
+    // from add is my wallet acc add
+    if (!toAddress || !amount) {
+        return res.status(400).send('Invalid request: Missing parameters');
+    }
     try{
 
-        if (type === "received_native_sol") {
-            await mintTokens(fromAddress, toAddress, amount);
-        } else {
+        if (type === "sol") {
+            await mintTokens( toAddress, amount);
+        } else if(type === "withdraw"){
             // What could go wrong here?
 
 
             // ans-- any other event could be sent to this endpoint and you'll still burn tokens and send sol.
-            await burnTokens(fromAddress, toAddress, amount);
-            await sendNativeTokens(fromAddress, toAddress, amount);
+            // await burnTokens(fromAddress, toAddress, amount);
+            // await sendNativeTokens(fromAddress, toAddress, amount);
         }
 
         res.send('Transaction successful');
---
+++ 5.lst/src/mintTokens.ts
@@ -0,0 +1,60 @@
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+
+import { Keypair } from "@solana/web3.js";
+import bs58 from "bs58";
+
+
+const LST_RATE = 960000000; 
+const base58Secret = process.env.PRIVATEKEY!;
+const payer = Keypair.fromSecretKey(bs58.decode(base58Secret));
+const mintAdd = new PublicKey(process.env.TOKEN_MINT!);
+
+
+
+const connection = new Connection("https://api.devnet.solana.com");
+export const mintTokens = async ( toAddress: string, amount: number) => {
+    console.log("Minting tokens");
+
+    // console.log(fromAddress);
+    console.log(toAddress);
+    // console.log()
+    const to = new PublicKey(toAddress);
+    // const from = new PublicKey(fromAddress);
+
+    const amt =  0.96*amount;
+
+    // create or get ata
+    const associatedAcc = await getOrCreateAssociatedTokenAccount(
+        connection,
+        payer,
+        mintAdd,
+        to
+    )
+
+    console.log("Ata acc created for user:",associatedAcc.address);
+
+    // mintTO
+    const signature = await mintTo(
+        connection,
+        payer,
+        mintAdd,
+        associatedAcc.address,
+        payer,
+        amt
+    );
+    console.log("mint tx sign:", signature);
+
+    console.log(`Minted ${amt} MySOL to ${to.toBase58()}`);
+
+
+
+}
+
+export const burnTokens = async (fromAddress: string, toAddress: string, amount: number) => {
+    console.log("Burning tokens");
+}
+
+export const sendNativeTokens = async (fromAddress: string, toAddress: string, amount: number) => {
+    console.log("Sending native tokens");
+}
